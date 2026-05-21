a14936cfc2b12b22abbb10543aac37aa7e451217
Hamzeh Sabbagh <hamzasabbagh215@gmail.com>
2026-05-21 23:06:50 +0300

    first commit

---
+++ backend/src/test/java/com/hamzeh/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzeh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
